ab5a2b5ad128d4a5a1d50836cde4acc96286a046
Ricardo Pérez <ricpelo@gmail.com>
2015-12-03 00:25:01 +0100

    Driver postgre en producción

--- application/config/production/database.php
+++ application/config/production/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => '',
 	'password' => '',
 	'database' => '',
-	'dbdriver' => 'mysqli',
+	'dbdriver' => 'postgre',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
