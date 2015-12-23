9c1b5dd6eb19f0f8645f0b1c34504f5fae2b65a6
Ricardo Pérez <ricpelo@gmail.com>
2015-12-23 20:20:03 +0100

    Añade REST Server

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,11 +1,14 @@
 <?php
 
-class MY_Controller extends CI_Controller {
+require APPPATH . '/third_party/restserver/libraries/REST_Controller.php';
+
+class MY_Controller extends REST_Controller {
 
     public function __construct()
     {
         parent::__construct();
-        if ($this->input->is_ajax_request())
+        if ($this->input->is_ajax_request() ||
+            $this->_detect_output_format() !== 'html')
         {
             $this->output->enable_profiler(FALSE);
         }
